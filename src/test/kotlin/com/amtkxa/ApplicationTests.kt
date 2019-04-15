068e1c373a64b942679f5bde562af163c262d4e3
amtkxa <scarlet0211@outlook.jp>
2019-04-16 00:33:59 +0900

    test: UserRepository のテストコードを追加

--- src/test/kotlin/com/amtkxa/ApplicationTests.kt
+++ src/test/kotlin/com/amtkxa/ApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.amtkxa.kotlinspringmultiproject
+package com.amtkxa
 
 import org.junit.Test
 import org.junit.runner.RunWith
---
+++ src/test/kotlin/com/amtkxa/domain/repository/UserRepositoryTests.kt
@@ -0,0 +1,50 @@
+package com.amtkxa.domain.repository
+
+import com.amtkxa.domain.entity.User
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.context.junit4.SpringRunner
+import java.time.LocalDateTime
+
+@RunWith(SpringRunner::class)
+@DataJpaTest
+class UserRepositoryTests {
+    @Autowired
+    lateinit var entityManager: TestEntityManager
+
+    @Autowired
+    lateinit var sut: UserRepository
+
+    @Test
+    @Sql(statements = ["INSERT INTO user (id, name, updated) VALUES (1, 'test', CURRENT_TIMESTAMP)"])
+    fun findById() {
+        val expected = entityManager.find(User::class.java, 1L)
+        val actual = sut.findById(expected.id).orElseGet { null }
+        assertThat(actual).isNotNull()
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun save() {
+        val updated = LocalDateTime.of(2019, 4, 15, 21, 39, 0)
+        val expected = User(name = "testuser", updated = updated)
+        sut.saveAndFlush(expected)
+        val actual = entityManager.find(User::class.java, expected.id)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    @Sql(statements = ["INSERT INTO user (id, name, updated) VALUES (1, 'test', CURRENT_TIMESTAMP)"])
+    fun delete() {
+        val expected = entityManager.find(User::class.java, 1L)
+        sut.deleteById(expected.id)
+        sut.flush()
+        val actual = entityManager.find(User::class.java, expected.id)
+        assertThat(actual).isNull()
+    }
+}
